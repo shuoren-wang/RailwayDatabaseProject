fc7dffc6280cd8dc92c630e343f8880e701dd836
BuildTools <mtbnunu@gmail.com>
2016-10-19 21:00:13 -0700

    gitignore and some SQL files

---
+++ sql/Views/vwAllLinesAndStations.sql
@@ -0,0 +1,16 @@
+CREATE VIEW vwAllLinesAndStations AS
+
+
+SELECT
+	l.id AS lineId,
+	l.linename,
+	ls.id AS linestopId,
+	ls.arrivalTime,
+	ls.stopsforduration,
+	s.id AS stationId,
+	s.stationname,
+	s.address
+FROM
+	line l
+	INNER JOIN linestops ls ON l.id = ls.forline_id
+	INNER JOIN stations s ON s.id = ls.locatedstation_id
