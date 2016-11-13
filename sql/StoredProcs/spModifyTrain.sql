51476bc7e214113ac4ca612618c55f9d64eb3706
BuildTools <mtbnunu@gmail.com>
2016-11-12 23:07:05 -0800

    re-enable modify and allow them to change only minor fields

--- sql/StoredProcs/spModifyTrain.sql
+++ sql/StoredProcs/spModifyTrain.sql
@@ -5,15 +5,7 @@ CREATE PROCEDURE spModifyTrain
 (IN
 in_trainNo INT,
 in_employeeId INT,
-in_runsM BOOL,
-in_runsT BOOL,
-in_runsW BOOL,
-in_runsTH BOOL,
-in_runsF BOOL,
-in_runsS BOOL,
-in_runsSU BOOL,
-in_TrainTypeId INT,
-in_LineId INT
+in_Active BOOL
 )
 
 
@@ -22,15 +14,7 @@ BEGIN
 
 UPDATE trains
 SET
-	`Runs_M` = in_runsM,
-	`Runs_T` = in_runsT,
-	`Runs_W` = in_runsW,
-	`Runs_TH` = in_runsTH,
-	`Runs_F` = in_runsF,
-	`Runs_S` = in_runsS,
-	`Runs_SU` = in_runsSU,
-	`TrainType_ID` = in_traintypeid,
-	`RunsLine_ID` = in_lineid,
+	`Active` = in_Active,
 	`UpdatedBy_EmployeeID` = in_employeeId
 
 WHERE
